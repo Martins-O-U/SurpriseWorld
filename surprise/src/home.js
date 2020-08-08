220174aa356e637a4cb362bdb9d20ec6128e2015
HP <martinsonyedikachi@gmail.com>
2020-08-08 14:36:40 +0100

    Form for landing page created

--- surprise/src/home.js
+++ surprise/src/home.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 
-function Home() {
+function Home(props) {
     const Name = {
         name: '',
     }
@@ -15,16 +15,25 @@ function Home() {
             ...message,
             [event.target.name]: event.target.value
         })
+        console.log(message)
 
+    }
+
+    const submitInput = (event) => {
+        event.preventDefault()
+        if (message.name.toLowerCase() === "chidinma" || "chimma" || "chidimma" || "mma") {
+            props.history.push('/success')
+        }
+        props.history.push('/failed')
     }
     return (
         <StyledDiv>
             <div className="welcome">
-                <p><span>Welcome</span> to the home of surprises</p>
+                <p><span>Welcome</span> to the home of surprises!</p>
                 <p>To experience today's surprise, please enter your first name below</p>
-                <form>
-                    <input name="name" placeholder="Your Name" onChange={handleChange} value={message.name} required />
-                    <button type="submit" />
+                <form onSubmit={submitInput}>
+                    <input name="name" placeholder="Enter Your Name" onChange={handleChange} value={message.name} required />
+                    <button type="submit"> Submit</button>
                 </form>
             </div>
         </StyledDiv>
@@ -40,11 +49,27 @@ const StyledDiv = styled.div`
 
     .welcome{
         margin-left: 30%;
-        padding-top: 40%;
+        padding-top: 35%;
 
         span{
             font-size: 30px;
             font-weight: 100px;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            width: 70%;
+            input{
+                height: 20px;
+            }
+            button{
+                margin-top: 3px;
+                align-self: flex-end;
+                border-radius: 10px;
+                padding: 2px 5px;
+                width: 80px;
+            }
         }
     }
 
